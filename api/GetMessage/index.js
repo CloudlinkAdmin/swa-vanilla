a5420a99f55d1b482ea8d3f01784f3031795e3ae
CloudlinkAdmin <60818101+CloudlinkAdmin@users.noreply.github.com>
2020-09-12 12:12:32 +0200

    2.nd commit

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,7 +1,8 @@
 module.exports = async function (context, req) {
     context.res = {
       body: {
-        text: "Hello from the API"
+        text: "Hello from the API",
+        text2: "Hello back"
       }
     };
   };
